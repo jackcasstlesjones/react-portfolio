6566c38cbda47f276201bc92e2c3d9401b1f70df
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 10:10:01 +0000

    style: add responsive design

--- src/Components/Projects/ProjectSection/ProjectSection.tsx
+++ src/Components/Projects/ProjectSection/ProjectSection.tsx
@@ -77,7 +77,8 @@ export default function ProjectSection({ project }: ProjectSectionProps) {
         />
       </div>
       <div className="absolute inset-0 -z-10 overflow-hidden"></div>
-      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+      <div className="max-w-xs sm:max-w-xl mx-auto grid grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10 sm:max-w-sm md:w-full">
+        {/* <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10 sm:max-w-sm md:w-full"> */}
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
