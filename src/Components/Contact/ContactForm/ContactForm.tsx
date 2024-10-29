0c2523d32a2873b8697390866d88e408fc7e0606
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 11:39:03 +0000

    chore: fix type errors

--- src/Components/Contact/ContactForm/ContactForm.tsx
+++ src/Components/Contact/ContactForm/ContactForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+// import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 export default function ContactForm() {
   return (
