02245029f7bd2dfe4521f138a120398aab651f9f
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-10-29 10:19:52 +0000

    refactor: change mobile nav

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -55,7 +55,7 @@ export default function Hero() {
             <button
               type="button"
               onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-50"
             >
               <span className="sr-only">Open main menu</span>
               <Bars3Icon aria-hidden="true" className="h-6 w-6" />
@@ -88,45 +88,48 @@ export default function Hero() {
           className="lg:hidden"
         >
           <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between">
+          <DialogPanel className=" fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between ">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  alt=""
-                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                  className="h-8 w-auto"
-                />
+                <img alt="" src={yellowPortrait} className="h-8 w-auto" />
               </a>
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                className="-m-2.5 rounded-md p-2.5 text-gray-50"
               >
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon aria-hidden="true" className="h-6 w-6" />
               </button>
             </div>
-            <div className="mt-6 flow-root">
+            <div className="mt-20 flow-root m-auto w-3/4 text-center">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+                      onClick={() => setMobileMenuOpen(false)}
+                      className="-mx-3 text-xl  rounded-lg px-3 py-2 font-semibold leading-7 text-white hover:bg-gray-50  flex items-center justify-center"
                     >
+                      {item.icon ? (
+                        <img
+                          src={item.icon}
+                          alt="LinkedIn Logo"
+                          className="h-6 w-6 mr-2"
+                        />
+                      ) : null}
                       {item.name}
                     </a>
                   ))}
                 </div>
                 <div className="py-6">
-                  <a
+                  {/* <a
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                   >
                     Log in
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
@@ -136,7 +139,11 @@ export default function Hero() {
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <img alt="" src={yellowPortrait} className="h-36 w-auto m-auto" />
+          <img
+            alt=""
+            src={yellowPortrait}
+            className="h-24 mb-8 w-auto m-auto md:mb-0 md:h-36"
+          />
 
           <div className="hidden sm:mb-8 sm:flex sm:justify-center z-10"></div>
           <div className="text-center">
